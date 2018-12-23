8abcd8042863f7038bd93c24faac48c771185caf
zhangbin670387 <670387260@qq.com>
2018-12-23 11:31:06 +0800

    zzz

--- src/gittest/Hello.java
+++ src/gittest/Hello.java
@@ -3,13 +3,12 @@ package gittest;
 public class Hello {
 
 	public static void main(String[] args) {
-		aaaaaa
-		ccccccc
-		bbbbbbbb
-		2222222222222222
-		11111111111111111
+		
 		System.out.println("world");
 		System.out.println("hello");
+		for(int i=0;i<10;i++){
+			System.out.println(i);
+		}
 	}
 
 }
